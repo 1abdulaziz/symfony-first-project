87e24b0314be48a9709a9f3a0ad3158f0549c4fc
abdulaziz zaid <aziz.sa03@gmail.com>
2022-11-10 15:37:51 +0300

    creted movie and actor entity and some chnage to learn

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -11,8 +11,10 @@ class MoviesController extends AbstractController
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
+
+        $movies = ['Avengers: Endgame', 'The Lion King', 'Frozen II', 'Toy Story 4', 'Captain Marvel'];
         return $this->render('movies/index.html.twig', [
-            'controller_name' => 'MoviesController',
+            'movies' => $movies,
         ]);
     }
 }
