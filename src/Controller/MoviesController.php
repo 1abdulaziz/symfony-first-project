36f046890531dbbb1716b1c688ba4620d54f50fa
abdulaziz zaid <aziz.sa03@gmail.com>
2022-11-13 23:14:31 +0300

    return data from Entity manager and show detiels for movies

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,19 +2,64 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movies')]
+    private $em;
+    private $movieRepository;
+
+    public function __construct(EntityManagerInterface $em , MovieRepository $movieRepository)
+    {
+        $this->em = $em;
+        $this->movieRepository = $movieRepository;
+
+    }
+
+    // example MovieRepository $entityManager
+    #[Route('/movies', name: 'app_movies' , methods: ['GET'])]
     public function index(): Response
     {
-
-        $movies = ['Avengers: Endgame', 'The Lion King', 'Frozen II', 'Toy Story 4', 'Captain Marvel'];
+        $movies = $this->movieRepository->findAll();
         return $this->render('movies/index.html.twig', [
             'movies' => $movies,
         ]);
     }
+    // example EntityManagerInterface $entityManager
+    #[Route('/movie/{movie}', name: 'movie', methods: ['GET'])]
+    public function movie($movie): Response
+    {
+        $movie = $this->em->getRepository(Movie::class)->find($movie);
+        // if $movie null
+        if (!$movie) {
+            throw $this->createNotFoundException(
+                'No movie found for id '.$movie
+            );
+        }
+        return $this->render('movies/show.html.twig', [
+            'movie' => $movie,
+        ]);
+    }
+
+//    /**
+//     * @Route("/movie/{movie}/edit", name="movie_edit", methods={"GET","POST"})
+//     */
+//    public function edit(Request $request, Movie $movie): Response
+//    {
+//        $form = $this->createForm(MovieType::class, $movie);
+//        $form->handleRequest($request);
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $this->getDoctrine()->getManager()->flush();
+//            return $this->redirectToRoute('movie', ['movie' => $movie->getId()]);
+//        }
+//        return $this->render('movies/edit.html.twig', [
+//            'movie' => $movie,
+//            'form' => $form->createView(),
+//        ]);
+//    }
 }
